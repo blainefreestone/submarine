351b191cbe0d552f3c20a4ac5e3fbfb27bb80ecc
Brock Davis <brockdavis246@gmail.com>
2026-04-02 13:03:41 -0600

    updated to work with serial

--- controls/joystick/src/joystick/comms/serial_link.py
+++ controls/joystick/src/joystick/comms/serial_link.py
@@ -97,14 +97,15 @@ class SerialLink:
         
         Args:
             port: Serial port path (e.g., "/dev/ttyUSB0")
-            baudrate: Communication baud rate
+            baudrate: Communication baud rateport
             timeout: Read timeout in seconds
         """
-        self.ser = serial.Serial(
-            port=port,
-            baudrate=baudrate,
-            timeout=timeout,
-        )
+        # self.ser = serial.Serial(
+        #     port=port,
+        #     baudrate=baudrate,
+        #     timeout=timeout,
+        # )
+        self.ser = serial.serial_for_url("socket://192.168.1.1:3001", timeout=1)
 
         self._running = True
         self._rx_thread = threading.Thread(
---
+++ controls/joystick/src/joystick/comms/test_motor.py
@@ -0,0 +1,4 @@
+import serial
+
+serial.serial_for_url("socket://192.168.1.1:3002", timeout=1)
+
